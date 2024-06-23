d816c71df24b1e1a782fde4dd65ad142a0e3b947
Valentine Lav <valentinelav98@gmail.com>
2024-06-23 16:22:18 +0200

    :white_check_mark: Add LoginControllerTests

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/LoginControllerTests.java
@@ -0,0 +1,26 @@
+package com.nnk.springboot.controllers;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void loginPageDisplayTest() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk());
+    }
+}
