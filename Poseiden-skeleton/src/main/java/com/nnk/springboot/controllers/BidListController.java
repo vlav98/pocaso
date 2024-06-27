356e503347ba24f1ed05bb6e04630bf219ef2dbc
Valentine Lav <valentinelav98@gmail.com>
2024-06-27 19:11:44 +0200

    :sparkles: Add BidList CRUD

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -2,61 +2,75 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.domain.User;
+import com.nnk.springboot.services.BidListService;
 import com.nnk.springboot.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
 
 
 @Controller
 public class BidListController {
-    // TODO: Inject Bid service
     @Autowired
     private UserService userService;
+    @Autowired
+    private BidListService bidListService;
+
+    @ModelAttribute
+    public void addAttributes(Model model) throws AccessDeniedException {
+        User connectedUser = userService.getAuthenticatedUser();
+        model.addAttribute("connectedUser", connectedUser);
+    }
 
     @RequestMapping("/bidList/list")
     public String home(Model model) throws AccessDeniedException {
-        User connectedUser = userService.getAuthenticatedUser();
-        model.addAttribute("connectedUser", connectedUser);
-        // TODO: call service find all bids to show to the view
+        model.addAttribute("bidLists", bidListService.findAll());
         return "bidList/list";
     }
 
     @GetMapping("/bidList/add")
-    public String addBidForm(BidList bid) {
+    public String addBidForm(BidList bidList, Model model){
+        model.addAttribute("bidList", bidList);
         return "bidList/add";
     }
 
     @PostMapping("/bidList/validate")
-    public String validate(@Valid BidList bid, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return bid list
-        return "bidList/add";
+    public String validate(@Valid BidList bidList, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "bidList/add";
+        }
+        bidListService.save(bidList);
+        return "redirect:/bidList/list";
     }
 
     @GetMapping("/bidList/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Bid by Id and to model then show to the form
+        BidList bidListToUpdate = bidListService.findById(id);
+        if (bidListToUpdate == null) {
+            throw new IllegalArgumentException("Invalid curvePoint id" + id);
+        }
+        model.addAttribute("bidList", bidListToUpdate);
         return "bidList/update";
     }
 
     @PostMapping("/bidList/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Bid and return list Bid
+        if (result.hasErrors()) {
+            return "bidList/update";
+        }
+        bidListService.update(id, bidList);
         return "redirect:/bidList/list";
     }
 
     @GetMapping("/bidList/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Bid by Id and delete the bid, return to Bid list
+        bidListService.deleteById(id);
         return "redirect:/bidList/list";
     }
 }
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerTests.java
@@ -0,0 +1,122 @@
+package com.nnk.springboot.controllers;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.services.BidListService;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BidListControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private BidListService mockBidListService;
+
+    @Test
+    @WithMockUser(username = "user")
+    public void bidListDisplayTest() throws Exception {
+        mockMvc.perform(get("/bidList/list"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("bidLists"))
+                .andExpect(model().attributeExists("connectedUser"))
+                .andExpect(view().name("bidList/list"));
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    public void bidListFormDisplayTest() throws Exception {
+        mockMvc.perform(get("/bidList/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("bidList/add"));
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    public void validateFormTest() throws Exception {
+        BidList validBidList = new BidList();
+        validBidList.setBidListId(1);
+        validBidList.setAccount("User");
+        validBidList.setType("Type test");
+        validBidList.setBidQuantity(2.0);
+
+        when(mockBidListService.save(any(BidList.class))).thenReturn(validBidList);
+
+        mockMvc.perform(post("/bidList/validate").with(csrf())
+                        .param("bidListId", String.valueOf(validBidList.getBidListId()))
+                        .param("account", validBidList.getAccount())
+                        .param("type", validBidList.getType())
+                        .param("bidQuantity", String.valueOf(validBidList.getBidQuantity())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/bidList/list"));
+
+        verify(mockBidListService).save(any(BidList.class));
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    public void updateFormDisplayWithValidIdTest() throws Exception {
+        BidList validBidList = new BidList();
+
+        when(mockBidListService.findById(anyInt())).thenReturn(validBidList);
+
+        mockMvc.perform(get("/bidList/update/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("bidList"))
+                .andExpect(view().name("bidList/update"));
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    public void updateFormDisplayWithInvalidIdTest() {
+        when(mockBidListService.findById(anyInt())).thenReturn(null);
+
+        assertThrows(Exception.class, () ->
+                mockMvc.perform(get("/bidList/update/1")));
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    public void updateBidListWithValidInputTest() throws Exception {
+        int validId = 1;
+
+        mockMvc.perform(post("/bidList/update/" + validId).with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/bidList/list"));
+    }
+
+    @Test
+    public void updateBidListWithValidInput_WithGuest_Test() throws Exception {
+        int validId = 1;
+
+        mockMvc.perform(post("/bidList/update/" + validId).with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    public void deleteBidListTest() throws Exception {
+        BidList bidList = new BidList();
+        when(mockBidListService.findById(anyInt())).thenReturn(bidList);
+
+        mockMvc.perform(get("/bidList/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/bidList/list"));
+
+        verify(mockBidListService).deleteById(anyInt());
+    }
+}
